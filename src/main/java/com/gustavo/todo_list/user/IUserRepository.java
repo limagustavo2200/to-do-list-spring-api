fc6bcba4621ebb8f55de52d70b470b9c79491b7c
limagustavo2200 <limagustavo2200@gmail.com>
2025-01-21 23:55:07 -0300

    first commit to-do-list application

---
+++ src/main/java/com/gustavo/todo_list/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.gustavo.todo_list.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    
+    UserModel findByUsername(String username);
+}
---
+++ src/main/java/com/gustavo/todo_list/user/UserController.java
@@ -0,0 +1,34 @@
+package com.gustavo.todo_list.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.badRequest().body("Usuario já existe!!!");
+        }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+}
+
